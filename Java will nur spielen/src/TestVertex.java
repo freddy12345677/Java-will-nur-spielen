2820da0aea930b98344db0796e64739b4d6d3399
axelc <axelc@LAPTOP-JC15ICH8>
2021-09-24 09:07:24 +0200

    commit 2

---
+++ Java will nur spielen/src/TestVertex.java
@@ -0,0 +1,14 @@
+
+public class TestVertex {
+
+	public static void main(String[] args) {
+		
+		Vertex v1 = new Vertex(17, 4);
+		Vertex v2 = new Vertex(42, 0); 
+		Vertex v3 = new Vertex(0, 0); 
+		
+		System.out.println(v2);
+		System.out.println(v1);
+	}
+
+}
