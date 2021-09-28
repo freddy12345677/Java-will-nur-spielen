d088db8d16e64e796e37eaa652ef48cfc2b4ef55
FrederikNoack <FrederikNoack@0-208-02>
2021-09-28 15:16:24 +0200

    28.9

--- Java will nur spielen/src/TestVertex.java
+++ Java will nur spielen/src/TestVertex.java
@@ -2,13 +2,16 @@
 public class TestVertex {
 
 	public static void main(String[] args) {
-		
+		int i = 0;
 		Vertex v1 = new Vertex(17, 4);
 		Vertex v2 = new Vertex(42, 0); 
-		Vertex v3 = new Vertex(0, 0); 
+		Vertex v3 = v1.skalarMult(42.42);
 		
 		System.out.println(v2);
 		System.out.println(v1);
+		System.out.println(v2.length()); // für mir mal von v2, length aus(methode)
+		
+		
 	}
 
 }
--- Java will nur spielen/src/Vertex.java
+++ Java will nur spielen/src/Vertex.java
@@ -1,20 +1,51 @@
 
-
 public class Vertex {
- public double x;
- public double y;
- 
- public Vertex(double x, double y) {
-	 this.x = x;
-	 this.y = y;
-    }
- public String toString() {
-	 
-	 return "x ="+x+", y ="+y;
-	 
-	 //oiwjdapowidj
-	 
-     }
- }
 
+	public double x;             //
+	public double y;
 
+	public Vertex(double x, double y) { //constructor entweder public oder void und dann direkt der Name
+		this.x = x;
+		this.y = y;
+	}
+
+	public String toString() {
+
+		return "x =" + x + ", y =" + y;
+		
+
+		// oiwjdapowidj
+
+	}
+	
+	public double length() {
+		return Math.sqrt(x*x+y*y);
+		
+	}
+	
+	public Vertex skalarMult(double s) {
+		
+		return new Vertex(x*s, y*s);
+			
+		
+	}
+	public void skalarMultMod(double s){
+		
+	x =s*x;
+	y =s*y;
+		
+		
+	}
+	public Vertex add(Vertex v2) {
+		
+	return new Vertex(x+v2.x, y+v2.y);	
+		
+	}
+	public void addMod(Vertex v2) {
+		
+	x=x+v2.x;
+	y=y+v2.y;
+		
+		
+	}
+}
